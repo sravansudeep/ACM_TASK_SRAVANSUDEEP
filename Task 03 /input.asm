b45557d0e60189eb260b00f6f87dffa8574448b5
sravansudeep <142815671+sravansudeep@users.noreply.github.com>
2023-11-17 19:57:44 +0530

    Update input.asm

--- Task 03 /input.asm
+++ Task 03 /input.asm
@@ -1,19 +1,19 @@
-	@i
-	M=1
-	@sum
-	M=0
+@i
+M=1
+@sum
+M=0
 (LOOP)
-	@i
-	D=M
-	@R0
-	D=D-M
-	@STOP
-	D;JGT
-	@i
-	D=M
-	@sum
-	M=D+M
-	@i
-	M=M+1
-	@loop
-	0;JMP
+@i
+D=M
+@R0
+D=D-M
+@STOP
+D;JGT
+@i
+D=M
+@sum
+M=D+M
+@i
+M=M+1
+@loop
+0;JMP
