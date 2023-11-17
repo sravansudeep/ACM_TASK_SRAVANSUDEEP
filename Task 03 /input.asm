bf3100bac145c624fa7edbc97bb176411ea14828
sravansudeep <142815671+sravansudeep@users.noreply.github.com>
2023-11-17 19:53:45 +0530

    Update input.asm

--- Task 03 /input.asm
+++ Task 03 /input.asm
@@ -1,21 +1,19 @@
-//computes RAM[1]=1+...RAM[0]
 	@i
-	M=1 //  i =1
+	M=1
 	@sum
-	M=0   //sum = 0
-
+	M=0
 (LOOP)
-	@i    // if i>RAM[0] goto STOP
+	@i
 	D=M
 	@R0
 	D=D-M
 	@STOP
-	D;JGT   //sum += i
+	D;JGT
 	@i
 	D=M
 	@sum
-	M=D+M  //  i++
+	M=D+M
 	@i
 	M=M+1
-	@loop  //  goto LOOP
+	@loop
 	0;JMP
